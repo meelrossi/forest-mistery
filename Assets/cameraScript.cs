c5210e1d21a8b4b3109789534f17f758e28c1bba
Agustin Mounier <agusmouni@gmail.com>
2017-12-08 17:25:39 -0300

    first commit

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+	public GameObject ball;
+	Vector3 offset;
+
+	void Start () {
+		offset = transform.position - ball.transform.position;
+	}
+
+	void LateUpdate () {
+		Vector3 newpos = new Vector3 (ball.transform.position.x + offset.x, ball.transform.position.y + offset.y, ball.transform.position.z + offset.z);
+		transform.position = newpos;
+	}
+}
---
+++ Assets/movingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+
+		if (Input.GetKey ("up")) {
+			GetComponent<Rigidbody> ().AddForce (Vector3.forward * 15f);
+		}
+
+		if (Input.GetKey ("left")) {
+			GetComponent<Rigidbody> ().AddForce (Vector3.left * 15f);
+		}
+
+		if (Input.GetKey ("right")) {
+			GetComponent<Rigidbody> ().AddForce (Vector3.right * 15f);
+		}
+
+		if (Input.GetKey ("down")) {
+			GetComponent<Rigidbody> ().AddForce (Vector3.back * 15f);
+		} 
+	}
+}
